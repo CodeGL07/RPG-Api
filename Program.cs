2813f5c8d68a763a8fbaf4cfc338164b7cb28022
CodeGL07 <lidrmanglauco9@gmail.com>
2026-04-07 22:29:40 -0300

    ft. Subiu Banco de Dados

--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Rpg_Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoLocal"));
+});
+
+
+
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
